899c3891cc6a5493913fa74f54bb861b18d7f0da
adity <adityroy672@gmail.com>
2025-07-04 20:39:34 +0530

    heading updated

--- src/heading/Heading.jsx
+++ src/heading/Heading.jsx
@@ -1,18 +1,18 @@
 import { motion } from "framer-motion";
 
-const Heading = ({subHeading,heading,description,buttonText}) => {
+const Heading = ({ subHeading, heading, description, buttonText }) => {
   return (
-    <section className=" px-4 md:px-10 py-10 my-10">
-      <div className="max-w-6xl  mx-auto flex flex-col md:flex-row items-start justify-between gap-12">
+    <section className=" sm:px-4 px-2 md:px-10 md:py-10 pt-6  ">
+      <div className="max-w-6xl  mx-auto flex flex-col md:flex-row items-start justify-between  gap-8 md:gap-12">
         {/* Left - Titles */}
         <motion.div
           initial={{ opacity: 0, x: -50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6 }}
-          className="md:w-1/2"
+          className="md:w-[55%]"
         >
-          <p className="italic text-lg  mb-2">  {subHeading}</p>
-          <h2 className="text-5xl font-serif font-semibold tracking-tight  ">
+          <p className="italic  md:text-3xl text-xl sm:text-2xl font-serif mb-2 "> {subHeading}</p>
+          <h2 className="md:text-6xl text-4xl sm:text-5xl w-full font-serif  font-extralight uppercase  ">
             {heading}
           </h2>
         </motion.div>
@@ -22,15 +22,16 @@ const Heading = ({subHeading,heading,description,buttonText}) => {
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6, delay: 0.2 }}
-          className="md:w-1/2"
+          className="md:w-[45%]"
         >
-          <p className=" leading-relaxed mb-6">
-           {description}
-          </p>
-
-          <button className="border border-white px-6 py-2 hover:bg-white hover:text-black font-semibold  transition duration-500">
-           {buttonText}
-          </button>
+          {description && (
+            <p className="font-serif font-light leading-relaxed mb-6">{description}</p>
+          )}
+          {buttonText && (
+            <button className="border text-xs font-medium cursor-pointer border-white px-8 py-3.5 hover:bg-white hover:text-black  transition duration-500">
+              {buttonText}
+            </button>
+          )}
         </motion.div>
       </div>
     </section>
